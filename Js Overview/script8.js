f4987fd872072c7fec72070804e03c2680cb4a3c
Om <godsire.om@example.com>
2026-02-21 12:06:30 +0530

    io

---
+++ Js Overview/script8.js
@@ -0,0 +1,13 @@
+let user=
+{
+    profile:[
+        {name:"Om"},
+        {name:"Amish"},
+        {name:"Harshal"}
+    ],
+    rating:[
+        {count:145},
+        {count:189},
+        {count:198}
+    ]
+}
