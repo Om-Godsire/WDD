0bd45ed766221804ddab92ef995eb4ca82ebf71d
Om <godsire.om@example.com>
2026-02-21 12:18:28 +0530

    8

--- Js Overview/script8.js
+++ Js Overview/script8.js
@@ -1,5 +1,4 @@
-let user=
-{
+let user={
     profile:[
         {name:"Om"},
         {name:"Amish"},
@@ -10,4 +9,17 @@ let user=
         {count:189},
         {count:198}
     ]
+}
+
+// get the following format of the ouput
+// output={
+//     "Om":{rating:145},
+//     "Om":{rating:145},
+//     "Om":{rating:145},
+// }
+for(var i=0;i<user.profile.length && i<user.rating.length;i++)
+{
+    const name=user.profile[i].name;
+    const rating=user.rating[i].count;
+    console.log(` ${name}: Rating:${rating}`)
 }
